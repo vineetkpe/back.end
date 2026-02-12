eb3918dbeb17677f00bfa5595a437bc956e05ffc
vineetkpe <vineetkpe@gmail.com>
2026-02-12 19:57:06 +0530

    addedd files

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-const express = require('express');
+const express = require('express'); // express imported
 
-express()
+const app = express() // server started and stored under a variable
+
+
+app.get("/login", (req,res) =>{
+    res.send("Login Here")
+})   // if it get any req on /login it will trigger this function
+
+app.get("/signup",(req,res) =>{
+    res.send("SignUpHere")
+})  // if it get any req on /signup it will trigger this function
+
+app.listen(3000)
